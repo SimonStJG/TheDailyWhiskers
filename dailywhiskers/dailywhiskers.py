51b6b8708096c5e28fabdad429cf08881d6e9b89
Simon StJG <Simon.StJG@gmail.com>
2020-09-06 11:16:25 +0100

    Learn to extract images from image galleries
    Hopefully this will make cloudwatch be quiet for a bit longer

--- dailywhiskers/dailywhiskers.py
+++ dailywhiskers/dailywhiskers.py
@@ -10,13 +10,9 @@ from collections import namedtuple
 from os import path
 
 
-logging.basicConfig()
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-logger.info("Initializing")
 
 config_file = path.join(path.dirname(path.dirname(__file__)), "config.json")
-
 requests_timeout = 10
 
 titles = [
@@ -40,7 +36,7 @@ last_names = [
     "the shrew killer", "the salmon shredder", "the vomiter"]
 
 MailgunConfig = namedtuple("MailgunConfig", ["url", "api_key", "from_address"])
-CatPicture = namedtuple("CatPicture", ["content", "content_type", "reddit_url"])
+CatPicture = namedtuple("CatPicture", ["url", "reddit_url"])
 
 
 def parse_config(filename):
@@ -79,41 +75,39 @@ def send(mailgun_config, to, html, image_name, image_content, image_content_type
     response.raise_for_status()
 
 
-def get_url(url, **kwargs):
-    response = requests.get(url, timeout=requests_timeout, **kwargs)
-    response.raise_for_status()
-    return response
-
-
 def get_cat_picture(json_child):
     try:
-        link_flair_text = json_child["data"]["link_flair_text"]
+        data = json_child["data"]
+        link_flair_text = data["link_flair_text"]
         if link_flair_text != "Cat Picture":
             logger.debug("Wrong link_flair_text: %s", link_flair_text)
             return None
 
-        # 1 because 0 is very low res.
-        url = json_child["data"]["preview"]["images"][0]["resolutions"][1]["url"]
+        if "preview" in data:
+            logger.debug("Found single preview image")
+            # 1 because 0 is very low res.
+            url = data["preview"]["images"][0]["resolutions"][1]["url"]
+        elif "gallery_data" in data:
+            logger.debug("Found preview image gallery")
+            first_media_id = data["gallery_data"]["items"][0]["media_id"]
+            # Number 3 looks like a reasonable resolution?  I'm not sure how these resolutions are chosen!
+            url = data["media_metadata"][first_media_id]["p"][3]["u"]
+        else:
+            raise ValueError("Not sure how to extract image from this JSON")
+
         # For a reason known only to the API designer, this is necessary
         url = url.replace("&amp;", "&")
 
-        reddit_url = "https://www.reddit.com" + json_child["data"]["permalink"]
-
-        #  Without the random headers, reddit gets very throttle-y.
-        response = get_url(url, headers={"user-agent": generate_random_user_agent()})
-        return CatPicture(content=response.content,
-                          content_type=response.headers["Content-Type"],
+        reddit_url = "https://www.reddit.com" + data["permalink"]
+        return CatPicture(url=url,
                           reddit_url=reddit_url)
     except (KeyError, IndexError):
         logger.exception("Failed to get cat pic from JSON, which was: \n%s", json_child)
         return None
 
 
-def cat_pictures():
-    children = json.loads(
-        get_url("https://www.reddit.com/r/cats/top.json?t=day",
-                headers={"user-agent": generate_random_user_agent()}
-                ).content.decode("UTF-8"))["data"]["children"]
+def get_cat_pictures(top_cats):
+    children = top_cats["data"]["children"]
 
     # + 1 because sometimes weird posts are stickied at the top
     for json_child in children[1:]:
@@ -133,11 +127,24 @@ def build_html(cat_name, image_file, reddit_url):
 
 
 def main():
-    logger.info("dailywhiskers started")
+    logging.basicConfig()
+    logger.setLevel(logging.DEBUG)
+
+    logger.info("Dailywhiskers started")
     (recipients, mailgun_config) = parse_config(config_file)
     logger.debug("Loaded config")
 
-    for recipient, cat_picture in zip(recipients, cat_pictures()):
+    session = requests.Session()
+    # Without this reddit gets very throttle-y
+    session.headers = {"user-agent": generate_random_user_agent()}
+
+    top_cats_resp = session.get("https://www.reddit.com/r/cats/top.json?t=day", timeout=requests_timeout)
+    top_cats_resp.raise_for_status()
+
+    for recipient, cat_picture in zip(recipients, get_cat_pictures(top_cats_resp.json())):
+        response = session.get(cat_picture.url, timeout=requests_timeout)
+        response.raise_for_status()
+
         logger.debug("Processing recipient: %s", recipient)
         cat_name = get_cat_name()
 
@@ -149,8 +156,8 @@ def main():
              to=recipient,
              html=build_html(cat_name, cat_pic_name, cat_picture.reddit_url),
              image_name=cat_pic_name,
-             image_content=cat_picture.content,
-             image_content_type=cat_picture.content_type)
+             image_content=response.content,
+             image_content_type=response.headers["Content-Type"])
 
 
 if __name__ == "__main__":
---
+++ test/test_get_cat_pictures.py
@@ -0,0 +1,60 @@
+import json
+from pathlib import Path
+
+from dailywhiskers.dailywhiskers import get_cat_pictures, CatPicture
+
+
+def test_get_cat_pictures():
+    with (Path(__file__).parent / "sample.json").open('r') as f:
+        top_cats = json.load(f)
+
+    assert list(get_cat_pictures(top_cats)) == [
+        CatPicture(
+            url='https://preview.redd.it/mb9i7wiwe1m51.jpg?width=216&crop=smart&auto=webp&s=8ede94d966718839c28e05617e7919de378c8c2b',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip817t/this_is_booker_booker_is_a_very_special_cat/'),
+        CatPicture(
+            url='https://preview.redd.it/m959oft2y0m51.jpg?width=640&crop=smart&auto=webp&s=3f53339038dd3a3ffa97c130660f82a37f31b3ba',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip6jji/my_boy_turned_7_today/'),
+        CatPicture(
+            url='https://preview.redd.it/5fqc3w33czl51.jpg?width=216&crop=smart&auto=webp&s=64bc745e77108269f9e2e9e42f723c08f7da5792',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip0vwe/my_boyfriend_got_me_the_cutest_bday_gift_reddit/'),
+        CatPicture(
+            url='https://preview.redd.it/k4cg80ef43m51.jpg?width=216&crop=smart&auto=webp&s=8b865405f8f93b4fbff5a60197eca1352e29454d',
+            reddit_url='https://www.reddit.com/r/cats/comments/ipc981/this_kitten_is_affecting_me_on_a_molecular_level/'),
+        CatPicture(
+            url='https://preview.redd.it/j14t68dy40m51.jpg?width=216&crop=smart&auto=webp&s=d738b9841d9a97136eb8369b1fb3e33c440af39d',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip3vxp/am_i_dead/'),
+        CatPicture(
+            url='https://preview.redd.it/4ibm6o4h71m51.jpg?width=216&crop=smart&auto=webp&s=b39059b863be65590f861acb19c0270ac2d25017',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip7di8/my_little_photogenic_boy/'),
+        CatPicture(
+            url='https://preview.redd.it/thu7jogqp0m51.jpg?width=216&crop=smart&auto=webp&s=f585f813bb1f9b97096e5824a6e82d0974966b66',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip5smf/kitten_at_the_animal_shelter_i_volunteer_at/'),
+        CatPicture(
+            url='https://preview.redd.it/qqcnuzzyf1m51.jpg?width=216&crop=smart&auto=webp&s=8374bb76402e9b862b937930a38623aa53b82928',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip846c/we_found_three_kittens_and_a_skinny_nursing_mom/'),
+        CatPicture(
+            url='https://preview.redd.it/fg1af30bkzl51.jpg?width=216&crop=smart&auto=webp&s=648659a3942ea48af10139d64f071c5314b5807d',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip1shc/when_you_let_a_stranger_pet_your_cat/'),
+        CatPicture(
+            url='https://preview.redd.it/5ynwpbbsm3m51.jpg?width=216&crop=smart&auto=webp&s=c3907e4b849957dc48ea3ed4f9248f101ddd1df1',
+            reddit_url='https://www.reddit.com/r/cats/comments/ipdd1v/this_is_navi_she_has_a_lot_of_ear_fluff/'),
+        CatPicture(
+            url='https://preview.redd.it/50422sqq4zl51.jpg?width=216&crop=smart&auto=webp&s=34a627374c85e297cd0f4a5c2f5042f51edb0506',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip03gl/found_an_orphaned_kitten_near_a_busy_intersection/'),
+        CatPicture(
+            url='https://preview.redd.it/3g44f6vevzl51.jpg?width=216&crop=smart&auto=webp&s=3938177b0c2cee6fcca194b31add2438f9b1b2bb',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip2y5g/my_handsome_man_is_missing_heres_a_cute_pic_of/'),
+        CatPicture(
+            url='https://preview.redd.it/yrys9lx74yl51.png?width=216&crop=smart&auto=webp&s=166eb6a94c03e6d801ae631e9312fb4a027e2780',
+            reddit_url='https://www.reddit.com/r/cats/comments/iow4kh/a_small_boi_i_drew/'),
+        CatPicture(
+            url='https://preview.redd.it/2f4ar98nf1m51.jpg?width=640&crop=smart&auto=webp&s=b76d64a5823214f2b9e65ffc2684f5ddae583b52',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip82ut/two_days_and_1700_later_my_murder_gremlin_is_home/'),
+        CatPicture(
+            url='https://preview.redd.it/koc7nrs8u3m51.jpg?width=640&crop=smart&auto=webp&s=e62bcc98cb1ad51f809cdfddaa767f39eb2f305a',
+            reddit_url='https://www.reddit.com/r/cats/comments/ipduc7/today_is_my_cat_chloes_birthday_today_she_turns_4/'),
+        CatPicture(
+            url='https://preview.redd.it/o3oim3nvm0m51.jpg?width=216&crop=smart&auto=webp&s=8c801618e977112acaab4765312dc36813d4ec14',
+            reddit_url='https://www.reddit.com/r/cats/comments/ip5jep/these_two_spend_all_day_fighting_then_give_me_10/'),
+    ]
